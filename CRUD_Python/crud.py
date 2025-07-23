8a7b832e157e1a56c04c362f0c7b2512ce444af1
Yane <yanesantos2001@hotmail.com>
2025-07-23 20:18:48 -0300

    ajustes para funcionar

--- CRUD_Python/crud.py
+++ CRUD_Python/crud.py
@@ -3,6 +3,13 @@ from Tabelas.recinto import recinto
 from Tabelas.cuidador import cuidador
 from CRUD.crud_recinto import criar_recinto
 from Tabelas.especie import especie
+from CRUD.crud_animal import criar_animal
+from CRUD.crud_eventosEducativos import criar_evento
+from datetime import datetime
+from Tabelas.eventosEducativos import eventosEducativos
+from Tabelas.animal import animal
+from CRUD.crud_animal import criar_animal
+
 
 db = functions(
     host="yane-2.local",
@@ -23,3 +30,12 @@ db.listar("cuidador")
 especie1 = especie("Lehonysys Fulanys", "Leão", "Savana africana")
 db.criar("especie", especie1)
 db.listar("especie")
+
+evento = eventosEducativos(1, "Parque em ação", "2025-07-08", 1)
+criar_evento(11, "Leões por todo o lado!", "2025-07-23", 60000)
+db.listar("eventosEducativos")
+
+animal = animal(1, "ricardo", "2025-07-08", "F", "Lehonysys Fulanys", 1)
+criar_animal(2, "ricardo", "2025-07-08", "F", "Lehonysys Fulanys", 1)
+db.listar("animal")
+
